2c99b66a4169a22dd2156b70142e9169cb91d157
JoseOrtizHdz <josejoaquin_1312@alumnos.uaslp.edu.mx>
2018-01-24 15:58:41 -0600

    kl

--- PaginaMemo/resources/views/welcome.blade.php
+++ PaginaMemo/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Qué Rayos?
+                    Laravel
                 </div>
 
                 <div class="links">
