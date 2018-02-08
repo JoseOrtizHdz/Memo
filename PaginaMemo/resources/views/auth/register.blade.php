d5aa57d9d458f0b4dfd6974b53b9b4a6715e37ef
JoseOrtizHdz <josejoaquin_1312@alumnos.uaslp.edu.mx>
2018-02-08 13:23:57 -0600

    Cambio de etiquetas en Registrar
    Se realizó el cambio de idioma en las etiquetas de registrar y recuperar contraseña

--- PaginaMemo/resources/views/auth/register.blade.php
+++ PaginaMemo/resources/views/auth/register.blade.php
@@ -5,13 +5,13 @@
     <div class="row justify-content-md-center mt-5">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Register</div>
+                <div class="card-header">Registrarse</div>
                 <div class="card-body">
                     <form role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group row">
-                            <label class="col-lg-4 col-form-label text-lg-right">Name</label>
+                            <label class="col-lg-4 col-form-label text-lg-right">Nombre</label>
 
                             <div class="col-lg-6">
                                 <input
@@ -30,7 +30,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label class="col-lg-4 col-form-label text-lg-right">E-Mail Address</label>
+                            <label class="col-lg-4 col-form-label text-lg-right">Correo electrónico</label>
 
                             <div class="col-lg-6">
                                 <input
@@ -50,7 +50,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label class="col-lg-4 col-form-label text-lg-right">Password</label>
+                            <label class="col-lg-4 col-form-label text-lg-right">Contraseña</label>
 
                             <div class="col-lg-6">
                                 <input
@@ -68,7 +68,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label class="col-lg-4 col-form-label text-lg-right">Confirm Password</label>
+                            <label class="col-lg-4 col-form-label text-lg-right">Confirmar contraseña</label>
 
                             <div class="col-lg-6">
                                 <input
@@ -88,7 +88,7 @@
                         <div class="form-group row">
                             <div class="col-lg-6 offset-lg-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Registrar
                                 </button>
                             </div>
                         </div>
